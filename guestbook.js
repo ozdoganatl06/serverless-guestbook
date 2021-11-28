435e275c9faba8883f2366587b859a12f722ce84
ozdoganatl06 <88246355+ozdoganatl06@users.noreply.github.com>
2021-11-28 20:43:35 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://33bc0bab.eu-gb.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://5aff4d7b.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
